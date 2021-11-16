577335eed54c6ac672684cf6243a2d3234d872be
Salman Ghouse <ghousemu@sheridancollege.ca>
2021-11-16 17:23:14 -0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+//Greet the user
+
+//array of different robots with different stats such as health, money, attack power
+
+//player health at start is 100
+
+
+//money is a variable starting is 200
+
+//while the player health is over 0 player and robots take turns attacking
+
+//if the player skips 20 money is deducted and new robot enters battle.
+
+
+
+//if player health is 0 or under game is over, prompted to try again
+
+//if robot health is 0 or under, a new robot enters the battle and round 2 is started once that robot is defeated round number increases
